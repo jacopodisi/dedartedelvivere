e2acb9b7dfcb1e54de9d0d41291a410167b3e41d
Jacopo Di Simone <jacopodisi@users.noreply.github.com>
2023-05-18 14:34:12 +0000

    dev environment update included jekyll, aspectcrop and imagemagick

--- images/cropandresize.sh
+++ images/cropandresize.sh
@@ -1,13 +1,14 @@
-folder=occasioni
-for i in $folder/promo_stressless.jpg
+folder=cucine
+for i in $folder/KALI03.jpg
 do
-   #aspectcrop -a 4:3 "$i" "${i%.jpg}_4x3.jpg"
-   # aspectcrop -a 16:9 "$i" "${i%.jpg}_thumb.jpg"
-   # aspectcrop -a 16:9 "$i" "${i%.jpg}_thumbbig.jpg"
-   # mogrify -path $folder/ -filter Triangle -define filter:support=2 -thumbnail '300>' -unsharp 0.25x0.25+8+0.065 -dither None -posterize 136 -quality 70 -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -colorspace sRGB -strip "${i%.jpg}_4x3.jpg"
-   # mogrify -path $folder/ -filter Triangle -define filter:support=2 -thumbnail '657>' -unsharp 0.25x0.25+8+0.065 -dither None -posterize 136 -quality 85 -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -colorspace sRGB -strip "${i%.jpg}_thumbbig.jpg"
-   mogrify -path $folder/ -filter Triangle -define filter:support=2 -thumbnail '657' -unsharp 0.25x0.25+8+0.065 -dither None -posterize 136 -quality 85 -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace plane -colorspace sRGB -strip "$i"
+   /workspaces/dedartedelvivere/aspectcrop -a 16:9 "$i" "${i%.jpg}_16x9.jpg"
+   /workspaces/dedartedelvivere/aspectcrop -a 16:9 "$i" "${i%.jpg}_16x9_thumb.jpg"
+   /workspaces/dedartedelvivere/aspectcrop -a 16:9 "$i" "${i%.jpg}_16x9_thumbbig.jpg"
+   mogrify -path $folder/ -filter Triangle -define filter:support=2 -thumbnail '300>' -unsharp 0.25x0.25+8+0.065 -dither None -posterize 136 -quality 70 -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -colorspace sRGB -strip "${i%.jpg}_16x9_thumb.jpg"
+   mogrify -path $folder/ -filter Triangle -define filter:support=2 -thumbnail '657>' -unsharp 0.25x0.25+8+0.065 -dither None -posterize 136 -quality 85 -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -colorspace sRGB -strip "${i%.jpg}_16x9_thumbbig.jpg"
+   mogrify -path $folder/ -filter Triangle -define filter:support=2 -thumbnail '1063>' -unsharp 0.25x0.25+8+0.065 -dither None -posterize 136 -quality 85 -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace plane -colorspace sRGB -strip "${i%.jpg}_16x9.jpg"
 done
+   # aspectcrop -a 4:3 "$i" "${i%.jpg}_4x3.jpg"
 # mogrify -path thumb -filter Triangle -define filter:support=2 -thumbnail 141 -unsharp 0.25x0.25+8+0.065 -dither None -posterize 136 -quality 82 -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -colorspace sRGB -strip thumb/*.jpg
 # mogrify -path thumb -filter Triangle -define filter:support=2 -thumbnail 303 -unsharp 0.25x0.25+8+0.065 -dither None -posterize 136 -quality 82 -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -colorspace sRGB -strip thumb/*.jpg
 # mogrify -path small -filter Triangle -define filter:support=2 -thumbnail '176' -unsharp 0.25x0.25+8+0.065 -dither None -posterize 136 -quality 85 -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace plane -colorspace sRGB -strip *.jpg
